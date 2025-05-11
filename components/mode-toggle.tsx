d6cd19222f0a5662f5a821288b469d5a3c84d4e3
Praveen Kumar <68219142+InfiniteCoder06@users.noreply.github.com>
2025-05-11 22:57:17 +0530

    💫 Mode Switcher animation

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -3,37 +3,44 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { NonAniButton } from "@/components/ui/button";
+import { SimpleButton } from "@/components/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { MotionDiv } from "./ui/motion";
 
 export function ModeToggle() {
 	const { setTheme } = useTheme();
 
 	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger className="absolute right-4 top-4" asChild>
-				<NonAniButton variant="outline" size="icon">
-					<Sun className="w-4 h-4 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute w-4 h-4 transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
-					<span className="sr-only">Toggle theme</span>
-				</NonAniButton>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>
-					Light
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>
-					Dark
-				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>
-					System
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<MotionDiv
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			transition={{ duration: 0.5, delay: 0.2 }}
+		>
+			<DropdownMenu>
+				<DropdownMenuTrigger className="absolute right-4 top-4" asChild>
+					<SimpleButton variant="outline" size="icon">
+						<Sun className="w-4 h-4 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
+						<Moon className="absolute w-4 h-4 transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
+						<span className="sr-only">Toggle theme</span>
+					</SimpleButton>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent align="end">
+					<DropdownMenuItem onClick={() => setTheme("light")}>
+						Light
+					</DropdownMenuItem>
+					<DropdownMenuItem onClick={() => setTheme("dark")}>
+						Dark
+					</DropdownMenuItem>
+					<DropdownMenuItem onClick={() => setTheme("system")}>
+						System
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</MotionDiv>
 	);
 }
