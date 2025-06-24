e6ce8210a73aff4e3a0f8906a62313ce2e65b864
Praveen Kumar <mallidipraveenkumarreddy@gmail.com>
2025-06-24 12:35:58 +0530

    ✨ Updater using Firebase Remote Config

--- app/api/version/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-  return new Response(JSON.stringify({ version: "0.2.0" }));
-}
--- components/updater.tsx
+++ components/updater.tsx
@@ -1,31 +1,70 @@
-export async function Updater({ version }: { version: string }) {
-  const data = await fetch(
-    process.env.NODE_ENV === "production"
-      ? "https://your-production-domain.com/api/version"
-      : "http://localhost:3000/api/version",
-  );
-  if (!data.ok) {
-    <div>
-      <p>Error fetching version information.</p>
-      <p>Status: {data.status}</p>
-      <p>Status Text: {data.statusText}</p>
-    </div>;
-  }
-  const serverVersion = await data.json();
+"use client";
 
+import { remoteConfig } from "@/lib/firebase";
+import { fetchAndActivate, getString } from "firebase/remote-config";
+import { useEffect, useState } from "react";
+import { AnimatePresenceWrapper, MotionDiv } from "./ui/motion";
+import { cn } from "@/lib/utils";
+import { AlertTriangle } from "lucide-react";
+
+export function Updater({ version }: { version: string }) {
+  const [isUpdateAvailable, setIsUpdateAvailable] = useState<boolean | null>(
+    null,
+  );
+
+  useEffect(() => {
+    fetchAndActivate(remoteConfig)
+      .then(() => {
+        const latestVersion = getString(remoteConfig, "version");
+        if (latestVersion && latestVersion !== version) {
+          console.log("New version available:", latestVersion);
+          setIsUpdateAvailable(true);
+        } else {
+          console.log("App is up to date.");
+          setIsUpdateAvailable(false);
+        }
+      })
+      .catch((err) => {
+        console.log("Error fetching remote config:", err);
+      });
+  }, [version]);
   return (
-    <div>
-      {version != serverVersion.version ? (
-        <>
-          <h2>Update Available</h2>
-          <p>
-            A new version of the app is available. Please refresh the page to
-            update.
-          </p>
-        </>
-      ) : (
-        <p>Your app is up to date.</p>
+    <AnimatePresenceWrapper>
+      {isUpdateAvailable === true && (
+        <div className="mb-4">
+          <MotionDiv
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            onClick={() => {
+              window.location.reload();
+            }}
+            transition={{ duration: 0.3 }}
+            className={cn(
+              "p-4 rounded-lg border flex items-center justify-between",
+              "bg-reda-ui border-reda-dim text-reda-dim",
+            )}
+          >
+            <div className="flex items-center gap-3">
+              <MotionDiv
+                animate={{
+                  scale: [1, 1.2, 1],
+                  rotate: [-5, 0, 5, 0],
+                  transition: { duration: 0.5, ease: "easeInOut" },
+                }}
+              >
+                {<AlertTriangle className={cn("h-6 w-6")} />}
+              </MotionDiv>
+              <div>
+                <h3 className={cn("font-medium")}>{"Update Available"}</h3>
+                <p className={cn("text-sm")}>
+                  {"Please refresh the page to get the latest version."}
+                </p>
+              </div>
+            </div>
+          </MotionDiv>
+        </div>
       )}
-    </div>
+    </AnimatePresenceWrapper>
   );
 }
